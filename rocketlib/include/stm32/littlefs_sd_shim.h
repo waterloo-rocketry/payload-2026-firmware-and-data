4a90110b233e66f6b603c13dea3a71103d1c0025
Rajeev Persaud <r3persau@uwaterloo.ca>
2026-03-30 18:24:30 -0400

    Add rocketlib as folder

---
+++ rocketlib/include/stm32/littlefs_sd_shim.h
@@ -0,0 +1,19 @@
+#ifndef ROCKETLIB_LITTLEFS_SHIM_H
+#define ROCKETLIB_LITTLEFS_SHIM_H
+
+#include "lfs.h"
+#include "stm32h7xx_hal_sd.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// LittleFS-compatible SD card interface functions
+int lfsshim_sd_mount(lfs_t *lfs, SD_HandleTypeDef *hsd, uint32_t first_block_offset);
+int lfsshim_sd_mount_mbr(lfs_t *lfs, SD_HandleTypeDef *hsd);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ROCKETLIB_LITTLEFS_SHIM_H
