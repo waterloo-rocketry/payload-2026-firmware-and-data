d81ef67e2b704fb4cadf8dd7656e1dfaf8b02deb
Rajeev Persaud <r3persau@uwaterloo.ca>
2026-04-12 10:23:08 -0400

    removed unnessary files and prepped code for writing to dAC

--- src/SPI/SPI_blinky.c
+++
@@ -1,9 +0,0 @@
-#include "SPI_ADC.h"
-#include "main.h"
-#include "stm32g4xx_hal.h"
-
-void SPI_BLINKY(void) {
-    HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET); 
-    HAL_Delay(500);
-    HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_SET);
-}
