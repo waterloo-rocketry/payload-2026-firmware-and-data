eec627fa0c5ec1e2b2be0e7df0808a4c814ffe24
Rajeev Persaud <r3persau@uwaterloo.ca>
2026-04-12 05:05:41 -0400

    LED debug again before and after tranmisson

--- src/SPI/SPI_ADC.c
+++ src/SPI/SPI_ADC.c
@@ -13,7 +13,14 @@ extern SPI_HandleTypeDef hspi1;
 void SPI_ADC_SendCommand(uint8_t cmd) {
     HAL_StatusTypeDef status;
     uint8_t data = cmd;
+    
+    // Turn LED on before transmission
+    HAL_GPIO_WritePin(GPIOA, LED2_Pin, GPIO_PIN_RESET);
+    
     status = HAL_SPI_Transmit(&hspi1, (uint8_t *)&data, sizeof(data), 1000);
+    
+    // Turn LED off after transmission
+    HAL_GPIO_WritePin(GPIOA, LED2_Pin, GPIO_PIN_SET);
     
     if (status != HAL_OK) {
         // Handle error
